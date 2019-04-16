87bbee991f6c7a5d6a961677a775d6c7e2533bf0
AnatoliAgeichik <43906902+AnatoliAgeichik@users.noreply.github.com>
2019-04-16 19:48:28 +0300

    Update z7.sh

--- lab2/z7.sh
+++ lab2/z7.sh
@@ -7,13 +7,13 @@ read c
 aa=$(($a*$a))
 bb=$(($b*$b))
 cc=$(($c*$c))
-if [ $(($aa + $bb)) == $cc ] || [ $(($aa + $cc)) == $bb] || [ $(($cc + $bb)) == $aa];
+if [[ $(($aa + $bb)) == $cc  ||  $(($aa + $cc)) == $bb ||  $(($cc + $bb)) == $aa ]];
 then
 	echo "This is rectangular triangle"
 fi
 
 
-if [ $(( $a + $b )) > $c ] && [ $(( $b + $c )) > $a ] && [ $(( $c + $a )) > $b ]; then
+if [[ $(( $a + $b )) > $c  &&  $(( $b + $c )) > $a  &&  $(( $c + $a )) > $b ]]; then
 	if [ $a = $b ] && [ $b = $c ]; then
 		echo "This is right"
 	elif [ $a = $b ] || [ $b = $c ] || [ $c = $a ]; then
