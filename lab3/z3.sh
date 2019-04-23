11370bf836158350510074d0f81a872a311bc2c2
AnatoliAgeichik <43906902+AnatoliAgeichik@users.noreply.github.com>
2019-04-23 19:15:39 +0300

    Update z3.sh

--- lab3/z3.sh
+++ lab3/z3.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-mkdir dir11 
+rm -rf dir11
+mkdir dir11
 
+rm -rf dir11/dir21 dir11/dir22 dir11/dir21/dir31
 mkdir dir11/dir21 dir11/dir22
 mkdir dir11/dir21/dir31
+rm -f dir11/text11.txt
 touch dir11/text11.txt
-	
-	
-	touch dir11/dir21/text21.txt  dir11/dir22/text220.txt dir11/dir22/text221.txt
-	ln -s dir11/text11.txt dir11/dir21/dir31
-	ln dir11/dir21/text21.txt dir11
-	ln dir11/dir21/text21.txt dir11/dir22
+
+rm -f dir11/dir21/text21.txt  dir11/dir22/text220.txt dir11/dir22/text221.txt	
+touch dir11/dir21/text21.txt  dir11/dir22/text220.txt dir11/dir22/text221.txt
+rm -f dir11/dir21/dir31/text41.txt
+touch dir11/dir21/dir31/text41.txt
+ln -s dir11/text11.txt dir11/dir21/dir31/text41.txt
+ln dir11/dir21/text21.txt dir11
+ln dir11/dir21/text21.txt dir11/dir22
